e0014999555fe339ddee0f653ecaa3eea6877edd
Stjepan <stephensemren1011@gmail.com>
2025-05-29 22:42:01 -0400

    fix: rename projects/pa → projects/Pa for Next.js routing

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // enable static “out/” folder export:
+  output: 'export',
+
   images: {
+    // disable the Image Optimization API so <Image> will just emit a normal <img>
+    unoptimized: true,
+
     domains: [
       'i.scdn.co',
       'image-cdn-ak.spotifycdn.com',
       'blend-playlist-covers.spotifycdn.com',
       'image-cdn-fa.spotifycdn.com',
       'miro.medium.com',
-      'cdn-images-1.medium.com'
+      'cdn-images-1.medium.com',
     ],
-    unoptimized: false
-  }
-};
+  },
+}
 
-export default nextConfig;
+export default nextConfig
